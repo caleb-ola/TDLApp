91382bfef08d80aeed0c1510584bba7f9daac1f5
caleb-ola <olajiire2@gmail.com>
2024-03-27 16:00:27 +0000

    feat: Implemented email service

--- utils/email.js
+++ utils/email.js
@@ -1,30 +1,82 @@
+const { convert } = require("html-to-text");
+
 const nodemailer = require("nodemailer");
 const process = require("process");
+const pug = require("pug");
+// const { fileURLToPath } = require("url");
+// const { dirname } = require("path");
 
-export default class Email {
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+class Email {
   constructor(user, url) {
     this.to = user.email;
+    this.from = process.env.API_EMAIL_FROM;
+    this.firstname = user.name.split(" ")[0];
+    this.url = url;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return nodemailer.createTransport({
+        host: process.env.BREVO_HOST,
+        port: process.env.BREVO_PORT,
+        auth: {
+          user: process.env.BREVO_USERNAME,
+          pass: process.env.BREVO_KEY,
+        },
+      });
+    } else {
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+    }
+  }
+
+  async send(template, subject) {
+    // eslint-disable-next-line no-undef
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`);
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async registerEmail() {
+    return this.send("verifyEmail", "Verify Your Email Address for TDLApp 📧");
   }
 }
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
 
-  const mailOptions = {
-    from: "TDLAPP <hello@tdlapp.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+//   const mailOptions = {
+//     from: "TDLAPP <hello@tdlapp.com>",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
 
-  await transporter.sendMail(mailOptions);
-};
+//   await transporter.sendMail(mailOptions);
+// };
 
-module.exports = sendEmail;
+module.exports = Email;
